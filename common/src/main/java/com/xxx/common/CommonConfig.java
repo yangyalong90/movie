6f63b0f91355df58f884baeace652f6eedb41740
yangyalong90@163.com <yangyalong90@163.com>
2020-11-27 23:31:29 +0800

    添加rest config

--- common/src/main/java/com/xxx/common/CommonConfig.java
+++ common/src/main/java/com/xxx/common/CommonConfig.java
@@ -1,15 +1,11 @@
 package com.xxx.common;
 
 import com.xxx.common.rest.RestConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(RestConfig.class)
 public class CommonConfig {
-
-    @Bean
-    public RestConfig restConfig(){
-        return new RestConfig();
-    }
 
 }
