4bdca82200c8deb73ace77115932ce93cb7ff46f
杨 <yangyalong90@163.com>
2021-04-20 21:19:35 +0800

    redis 测试

--- clients/user/user-core/src/main/java/com/xxx/movie/user/controller/UserInfoController.java
+++ clients/user/user-core/src/main/java/com/xxx/movie/user/controller/UserInfoController.java
@@ -4,10 +4,11 @@ import com.xxx.common.model.ApiResult;
 import com.xxx.common.version.annotations.ApiVersion;
 import com.xxx.movie.user.common.entity.UserInfo;
 import com.xxx.movie.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/info")
@@ -15,8 +16,11 @@ public class UserInfoController {
 
     private UserService userService;
 
-    public UserInfoController(UserService userService) {
+    private RedisTemplate redisTemplate;
+
+    public UserInfoController(UserService userService, RedisTemplate redisTemplate) {
         this.userService = userService;
+        this.redisTemplate = redisTemplate;
     }
 
     @ApiVersion("0.3")
@@ -26,6 +30,14 @@ public class UserInfoController {
         info.setId(id);
         info.setName("0.3");
         return info;
+    }
+
+    @GetMapping("/demo/redis")
+    public ApiResult redis(@RequestParam("script") String script){
+        DefaultRedisScript defaultRedisScript = new DefaultRedisScript(script, String.class);
+        Object execute = redisTemplate.execute(defaultRedisScript, Collections.emptyList());
+
+        return ApiResult.error();
     }
 
     @ApiVersion(value = "0.1", oldFullPath = "/b/{id}")
